0b419935090c427fb13e9fd5ef6f6640f0f232d7
mackenziekira <mackenzie@joinrivet.com>
2022-03-15 18:10:01 -0700

    Actually clear file names from intent
    Rather than setting module-level `isClear` boolean, which would only allow the app to receive & respond to one share.
    
    This change prevents the issue on Android of the app 1) receives a share intent, 2) user then opens photo picker, causing app to go to bg and back to fg, 3) triggers share sheet to reopen, because original share intent info has not been properly cleared
    
    https://github.com/ajith-ab/react-native-receive-sharing-intent/issues/110#issuecomment-1009007819

--- src/ReceiveSharingIntent.ts
+++ src/ReceiveSharingIntent.ts
@@ -33,7 +33,7 @@ class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     }
 
     clearReceivedFiles(){
-        this.isClear = true;
+        ReceiveSharingIntent.clearFileNames();
     }
 
     
